5775ca8c69a7d4dc2fb9b4b3a4611c647037fdf7
JiangXiu <JiangXiu11200@gmail.com>
2025-04-26 15:25:35 +0800

    feat (header): implement dynamic breadcrumb navigation based on current route

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,15 +1,32 @@
-import React from "react"
 import { BreadCrumb } from "primereact/breadcrumb"
-import { LiaUserCircle } from "react-icons/lia"
-import { Menu } from "primereact/menu"
-import { useState, useRef } from "react"
-import "./header.css"
 import { Button } from "primereact/button"
+import { Menu } from "primereact/menu"
+import React, { useMemo, useRef } from "react"
+import { LiaUserCircle } from "react-icons/lia"
+import { useLocation } from "react-router-dom"
+
+import "./header.css"
 
 function Header() {
+    const location = useLocation()
     const home = { icon: "pi pi-home", url: "/" }
-    const items = [{ label: "FaceRecognition" }]
     const menuRight = useRef(null)
+
+    const breadcrumbMap = {
+        "/": [{ label: "Face Recognition" }],
+        "/user_registration": [{ label: "User Registration" }],
+        "/alarm_logs": [{ label: "Alarm Logs" }],
+        "/activity_logs": [{ label: "Activity Logs" }],
+        "/settings/recognition": [{ label: "Settings" }, { label: "Recognition" }],
+        "/settings/accounts": [{ label: "Settings" }, { label: "Accounts" }],
+        "/settings/system": [{ label: "Settings" }, { label: "System" }],
+    }
+
+    const items = useMemo(() => {
+        console.log("location", location)
+        return breadcrumbMap[location.pathname] || []
+    }, [location.pathname])
+
     const menu_item = [
         { label: "New", icon: "pi pi-fw pi-plus" },
         { label: "Delete", icon: "pi pi-fw pi-trash" },
