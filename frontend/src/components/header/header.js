31166de5c1e02e459f613a3ccb0ac47bd6c6b1ae
JiangXiu <JiangXiu11200@gmail.com>
2025-07-18 08:30:57 +0800

    feat (header): enhance user menu with avatar and update styles

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,16 +1,17 @@
+import React, { useMemo, useRef, useState } from "react"
+import { Avatar } from "primereact/avatar"
 import { BreadCrumb } from "primereact/breadcrumb"
-import { Button } from "primereact/button"
 import { Menu } from "primereact/menu"
-import React, { useMemo, useRef } from "react"
-import { LiaUserCircle } from "react-icons/lia"
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 import "./header.css"
 
 function Header() {
-  const location = useLocation()
+  const navigate = useNavigate()
   const home = { icon: "pi pi-home", url: "/" }
+  const location = useLocation()
   const menuRight = useRef(null)
+  const [userPhotoUrl, setUserPhotoUrl] = useState(null)
 
   const breadcrumbMap = {
     "/": [{ label: "Face Recognition" }],
@@ -32,8 +33,35 @@ function Header() {
   }, [location.pathname])
 
   const menu_item = [
-    { label: "New", icon: "pi pi-fw pi-plus" },
-    { label: "Delete", icon: "pi pi-fw pi-trash" },
+    {
+      template: (item, options) => {
+        return (
+          <div
+            className="p-3 gap-3 d-flex align-items-center"
+            style={{ borderBottom: "1px solid #ddd" }}
+          >
+            <Avatar
+              image={userPhotoUrl ?? "/image/cat.jpg"}
+              shape="circle"
+              size="large"
+            />{" "}
+            {/* The backend returns the S3 URL */}
+            <div>
+              <div>{"Jonas"}</div> {/* // The backend return user name */}
+            </div>
+          </div>
+        )
+      },
+      disabled: true,
+    },
+    { label: "Help & Support", icon: "pi pi-question-circle" },
+    {
+      label: "Logout",
+      icon: "pi pi-sign-out",
+      command: () => {
+        navigate("/login"), window.location.reload()
+      },
+    },
   ]
   return (
     <div className="row header-container">
@@ -41,20 +69,19 @@ function Header() {
         <BreadCrumb className="custom-breadcrumb" model={items} home={home} />
       </div>
       <div className="col right-layout">
-        <div>
+        <div className="avatar-wrapper user_menu_btn">
+          <Avatar
+            image={userPhotoUrl ?? "/image/cat.jpg"}
+            shape="circle"
+            size="large"
+            onClick={(event) => menuRight.current.toggle(event)}
+          />
           <Menu
             id="user_menu"
             model={menu_item}
             popup
             ref={menuRight}
             popupAlignment="right"
-          />
-          <Button
-            id="user_menu"
-            className="header-button p-button-rounded p-button-text p-button-icon-only p-0"
-            icon={<LiaUserCircle className="user-icon" />}
-            onClick={(event) => menuRight.current.toggle(event)}
-            aria-controls="user_menu"
           />
         </div>
       </div>
