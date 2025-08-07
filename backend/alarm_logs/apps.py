6c16d19cd81d681163953dc726027b76c2d023b4
JiangXiu <JiangXiu11200@gmail.com>
2025-08-07 15:56:21 +0800

    style: code formatting

--- backend/alarm_logs/apps.py
+++ backend/alarm_logs/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AlarmLogsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alarm_logs'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "alarm_logs"
