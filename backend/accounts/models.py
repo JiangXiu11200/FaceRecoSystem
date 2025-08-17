82e34c533e70ab1b56c6869a2c6a25230a35a373
JiangXiu <JiangXiu11200@gmail.com>
2025-08-17 11:24:14 +0800

    fix (account): database modification
    - allow the first_name, last_name and email fields to be blank or empty

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,15 +5,15 @@ class UserProfile(models.Model):
     account = models.CharField(max_length=64, unique=True)
     password = models.CharField(max_length=128)
     keep_expiration_days = models.IntegerField(default=1)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    email = models.EmailField(max_length=254)
+    first_name = models.CharField(max_length=64, blank=True, null=True)
+    last_name = models.CharField(max_length=64, blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
     profile_picture_url = models.URLField(max_length=500, blank=True, null=True)
     description = models.TextField(max_length=50, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    user_groups = models.ManyToManyField("UserGroup", related_name="users", null=True, blank=True)
+    user_groups = models.ManyToManyField("UserGroup", related_name="users", blank=True)
 
     def save(self, *args, **kwargs):
         if self.account:
