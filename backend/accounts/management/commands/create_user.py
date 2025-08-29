a8b833e1f65ffeeddd5599e1238e441e05aaec1c
JiangXiu <JiangXiu11200@gmail.com>
2025-08-29 17:45:49 +0800

    fix(accounts): Database default data pk key conflict issue

--- backend/accounts/management/commands/create_user.py
+++ backend/accounts/management/commands/create_user.py
@@ -7,7 +7,7 @@ from accounts.utils.verify_passward import make_hashed_password
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Create a default user group if it doesn't exist
-        default_group, created = UserGroup.objects.get_or_create(defaults={"group_name": "Administrator"}, id=1)
+        default_group, created = UserGroup.objects.get_or_create(defaults={"group_name": "Administrator"})
 
         # Get all system apps
         system_apps = SystemApps.objects.all()
